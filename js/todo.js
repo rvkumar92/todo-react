8412c7df1ff83eb9dedcfc443094e1d207d19f12
rvkumar92 <ind.vijayworldz@gmail.com>
2016-02-22 02:07:48 +0530

    added removing and adding note to bulletin functionality

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,12 @@
-var DATA = {
-    notes: [
-        "first",
-        "second",
-        "third"
-    ]
-};
 var Todo = React.createClass({
     getInitialState(){
         return({editing: false})
     },
     editNote(){
         this.setState({editing: true})
-
     },
     deleteNote(){
-        alert("deleting note")
+        this.props.onRemove(this.props.index)
     },
     saveNote(){
         this.setState({editing: false})
@@ -30,7 +22,7 @@ var Todo = React.createClass({
     },
     renderForm(){
         return(<div className="note">
-            <textarea className="form-control">{this.props.children}</textarea>
+            <textarea defaultValue={this.props.children} ref="noteValue" className="form-control"></textarea>
             <button className="btn btn-primary glyphicon glyphicon-floppy-disk" onClick={this.saveNote}/>
             </div>);
     },
@@ -46,9 +38,31 @@ var Board = React.createClass({
     getInitialState(){
       return ({notes: []})
     },
+    addNote(text){
+        var noteArray = this.state.notes;
+        noteArray.push(text);
+        this.setState({notes: noteArray})
+    },
+    updateNote(note,index){
+        console.log("remove");
+        var arrayNote = this.state.notes;
+        noteArray[i] = note;
+        this.setState({notes: noteArray[i]})
+    },
+    removeNote(index){
+        var noteArray = this.state.notes;
+        noteArray.splice(index,1);
+        this.setState({notes: noteArray})
+    },
+    eachNote(note,i){
+        return(
+            <Todo key={i} index={i} onChange={this.updateNote} onRemove={this.removeNote}>{note}</Todo>
+        )
+    },
     render(){
-        return (<div>
-            <Todo>HelloWorld</Todo>
+        return (<div className="board">
+            {this.state.notes.map(this.eachNote)}
+            <button className="btn btn-sm glyphicon glyphicon-plus" onClick={this.addNote.bind(null,"New Note...")}/>
             </div>);
     }
 });
